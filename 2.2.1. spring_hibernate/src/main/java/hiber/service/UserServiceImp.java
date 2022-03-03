c98c4f6d780114745e14f72695f31eaf85d98615
YuretsGrebets <eurko25@mail.ru>
2022-03-03 21:16:05 +0300

    comment

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,9 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Override
+   public User getUser (String series, int model) {
+      return userDao.getUser(series, model);
+   }
+
 }
